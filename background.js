4cd6737c6132280d445afc2e5ca5e9fe2048efc1
Vik Boyechko <vik.boyechko@gmail.com>
2025-02-06 15:21:42 -0600

    remove save options from background.js

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
+// Initialize extension when installed
+chrome.runtime.onInstalled.addListener(() => {
+  // Initialize storage with empty price history
+  chrome.storage.local.set({
+    priceHistory: {}
+  });
+});
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'open-popup') {
     chrome.windows.create({
@@ -7,31 +15,5 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       height: 200,
     });
     return true;
-  }
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'toggle-option') {
-    chrome.storage.local.get(['trackingOptions'], function (result) {
-      const options = result.trackingOptions || {
-        trackDomain: false,
-        trackPage: false,
-        trackingEnabled: false,
-      };
-
-      switch (request.option) {
-        case 'track-domain':
-          options.trackDomain = !options.trackDomain;
-          break;
-        case 'track-page':
-          options.trackPage = !options.trackPage;
-          break;
-        case 'toggle-tracking':
-          options.trackingEnabled = !options.trackingEnabled;
-          break;
-      }
-
-      chrome.storage.local.set({ trackingOptions: options });
-    });
   }
 });
